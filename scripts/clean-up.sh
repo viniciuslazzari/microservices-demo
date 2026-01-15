58223b566e49fe871c0716162b10a983aa455f69
laurakrs <laurakrs>
2026-01-15 23:12:24 +0100

    Improve clean-up script

--- scripts/clean-up.sh
+++ scripts/clean-up.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Delete services
-kubectl delete services --all --namespace=monitoring
-
-# Delete deployments
-kubectl delete deployments --all --namespace=monitoring
+# Delete  monitoring resources
+if kubectl get namespace monitoring 2>/dev/null; then
+  echo "Deleting monitoring resources..."
+  cd ../monitoring
+  kubectl delete -f .
+else
+  echo "Monitoring namespace doesn't exist. Skipping monitoring resource deletion."
+fi
 
 # Delete resources deployed with Kustomize
 cd ../kustomize
